a68f09692c5deea187e68ca4e3503feb4e8dbdff
CSSLR <mrr.dilrmp@gmail.com>
2026-02-11 16:22:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-// Scroll animation
-const faders = document.querySelectorAll(".fade");
+const faders=document.querySelectorAll(".fade");
 
-const appearOptions = {
-  threshold: 0.2
-};
-
-const appearOnScroll = new IntersectionObserver(function(entries, observer) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) return;
-    entry.target.classList.add("show");
-    observer.unobserve(entry.target);
-  });
-}, appearOptions);
-
-faders.forEach(fader => {
-  appearOnScroll.observe(fader);
+const observer=new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
 });
+},{threshold:0.15});
+
+faders.forEach(el=>observer.observe(el));
